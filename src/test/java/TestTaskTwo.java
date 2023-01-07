a692e39f9a9c2a0df5352614fbba5edc5c956703
nazar12314 <kononenkonazar47@gmail.com>
2023-01-07 14:46:01 +0200

    fixed linting

--- src/test/java/TestTaskTwo.java
+++ src/test/java/TestTaskTwo.java
@@ -39,35 +39,4 @@ public class TestTaskTwo {
                         "System.out.println(nums);"
                 );
     }
-
-    @Test
-    public void testTimedDecorator() throws SQLException {
-        Document document = new SmartDocument(
-                "gs://ucu-bucket/Знімок екрана 2022-12-01 о 14.11.41.png"
-        );
-        document = new TimedDocument(document);
-
-        Assertions.assertEquals(
-                document.parse(),
-                "package org.example;\n"
-                        +
-                        "import java.util.*;\n"
-                        +
-                        "public class Main {\n"
-                        +
-                        "public static void main(String[] args) {\n"
-                        +
-                        "List<List<Integer>> nums = "
-                        +
-                        "new ArrayList<>(List.of(List.of(1, 2), List.of(1, 1), List.of(4, 3)));\n"
-                        +
-                        "}\n"
-                        +
-                        "}\n"
-                        +
-                        "nums.sort (Comparator.comparing Int (o -> o.get(1)));\n"
-                        +
-                        "System.out.println(nums);"
-        );
-    }
 }
